f3d33eec2ad2fd8266967ff95284238257253d41
AlvaroPani <alvaro.paniaguac@edu.uah.es>
2026-04-15 15:55:21 +0200

    carpetas vacias con archivos de rellenos nullos

---
+++ src/dp_seleccion.py
@@ -0,0 +1,13 @@
+"""
+Este módulo contiene la función de seleccion de pedidos segun:
+    -Cada pedido se representa como una estructura: 
+        + Pedido = (id, peso, beneficio) 
+    -Dado: 
+        + Una lista de pedidos disponibles. 
+        + Una capacidad máxima de carga C. 
+    -Se debe seleccionar el subconjunto de pedidos que: 
+        + No exceda C. 
+        + Maximice el beneficio total.
+
+Solucion realizada medienate Programación Dinámica,
+"""
---
+++ src/main.py
@@ -0,0 +1,15 @@
+"""
+Este es el modulo principal de la aplicacion, este contiene:
+    La funcion main encargada de llamar a las demas funciones y ejecutar el programa.
+    ...
+    
+"""
+
+
+def main():
+    print("Hello, World!")
+
+
+
+if __name__ == "__main__":
+    main()
