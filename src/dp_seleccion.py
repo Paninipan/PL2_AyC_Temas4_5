2d59aea4344152cb1eed5e6b633c55285dce3350
AlvaroPani <alvaro.paniaguac@edu.uah.es>
2026-04-15 17:14:23 +0200

    estructuras

--- src/dp_seleccion.py
+++ src/dp_seleccion.py
@@ -2,12 +2,29 @@
 Este módulo contiene la función de seleccion de pedidos segun:
     -Cada pedido se representa como una estructura: 
         + Pedido = (id, peso, beneficio) 
-    -Dado: 
+    -Entradas: 
         + Una lista de pedidos disponibles. 
         + Una capacidad máxima de carga C. 
     -Se debe seleccionar el subconjunto de pedidos que: 
         + No exceda C. 
         + Maximice el beneficio total.
+        + Salida:
+            + Diccionario:
+                + Clave: id_camion
+                + Valor: Tupla "Tripelata -> + punto de entrega"??
+                    * Beneficio total
+                    * Lista de pedidos asignados a ese camión.
 
-Solucion realizada medienate Programación Dinámica,
+Solucion realizada medienate Programación Dinámica, descente recursivo.
 """
+
+def seleccion_pedidos(pedidos, capacidad) -> dict:
+    
+    def seleccion_pedidos_cache(pedidos, capacidad, cache):
+        if len(pedidos) == 0 or capacidad <= 0:
+            return 0, []
+        if pedidos == None:
+            return 0, []
+    
+
+    return diccionario_entregas
--- src/main.py
+++ src/main.py
@@ -5,6 +5,34 @@ Este es el modulo principal de la aplicacion, este contiene:
     
 """
 
+"""
+Estructuras de datos y algoritmos implementados:
+
+    Estructuras de datos:
+        - Matriz de pedidos (Nx3 pedidos) :
+            + Fila: Pedido 
+            + Columnas: id_pedido, peso, beneficio
+        
+        - Capacidad de carga: C (entero) representando la capacidad máxima de carga de cada vehículo.
+
+        - Diccionario de camiones y pedidos asignados: ???
+            + Clave: id_camion 
+            + Valor: Tupla "Tripelata -> + punto de entrega"
+                * Beneficio total
+                * Lista de pedidos asignados a ese camión.
+        
+        - Matriz de distancias entre puntos de entrega (grafo ponderado) {k*k nodos}:
+            + Fila: Punto o nodo de entrega
+            + Columna: Distancia a otro nodo
+        
+        - Lista de entregas (longitud k nodos):
+            + Nodo de entrega 
+   
+    Algoritmos:
+        - Selección de pedidos (Programación Dinámica)
+        - Backtracking para encontrar la ruta óptima 
+"""
+
 
 def main():
     print("Hello, World!")
