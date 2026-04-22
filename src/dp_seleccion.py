50c419fa50d225279b6179582a96fb0197835107
AlvaroPani <alvaro.paniaguac@edu.uah.es>
2026-04-22 16:24:21 +0200

    mochila

--- src/dp_seleccion.py
+++ src/dp_seleccion.py
@@ -1,30 +1,107 @@
 """
 Este módulo contiene la función de seleccion de pedidos segun:
     -Cada pedido se representa como una estructura: 
-        + Pedido = (id, peso, beneficio) 
+        + Pedido = (id, peso, beneficio, destino)
     -Entradas: 
-        + Una lista de pedidos disponibles. 
+        + Una matriz de pedidos disponibles.
         + Una capacidad máxima de carga C. 
     -Se debe seleccionar el subconjunto de pedidos que: 
         + No exceda C. 
         + Maximice el beneficio total.
         + Salida:
-            + Diccionario:
-                + Clave: id_camion
-                + Valor: Tupla "Tripelata -> + punto de entrega"??
-                    * Beneficio total
-                    * Lista de pedidos asignados a ese camión.
+            -Lista de pedidos asignados:
+                + Valor: Tupla 3 valors
+                    * Id_Pedido
+                    * Destino
+                    * Beneficio
 
 Solucion realizada medienate Programación Dinámica, descente recursivo.
+    + Ordenar por beneficio
+    + Asignar si peso < capacidad
+    + Return si capacidad == 0 o matriz vacia.
 """
 
-def seleccion_pedidos(pedidos, capacidad) -> dict:
+def seleccion_pedidos(pedidos, capacidad) -> list:
     
     def seleccion_pedidos_cache(pedidos, capacidad, cache):
-        if len(pedidos) == 0 or capacidad <= 0:
-            return 0, []
-        if pedidos == None:
-            return 0, []
-    
 
-    return diccionario_entregas
+        longitud = len(pedidos)
+        print(longitud)
+        if longitud == 0 or capacidad <= 0:
+            return []
+        elif longitud == 1:
+            return pedidos if (pedidos[0][1] <= capacidad) else []
+        else:
+            for i in range(longitud):
+                if capacidad <= 0:
+                    return cache
+                elif pedidos[i][1] <= capacidad:
+                    capacidad -= pedidos[i][1]
+                    cache.append(pedidos[i])
+            return cache
+
+    cache = []
+    #Ordenamos los pedidos por el beneficio [2]
+    # Ya que pedido = (id, peso, beneficio, destino))
+    sorted(pedidos, key=lambda x: x[2], reverse=True)
+    return seleccion_pedidos_cache(pedidos,capacidad, cache)
+
+
+def seleccion_pedidos(pedidos, capacidad) -> list:
+    cache = {}
+
+    def seleccion_pedidos_dinamico(id_dic, capcidad):
+        # Caso Base: No quedan pedidos o no hay capacidad
+        if id_dic == len(pedidos) or capcidad <= 0:
+            return 0, []
+
+        # Comprobamos que la tupla este ya analizada
+        existe = (id_dic, capcidad)
+        if existe in cache:
+            return cache[existe]
+
+        # Si no lo incluimos sea como sea
+        beneficio_sin, lista_sin = seleccion_pedidos_dinamico(id_dic + 1, capcidad)
+
+        # Incluir el pedido actual si es posible
+        peso_actual = pedidos[id_dic][1]
+        beneficio_actual = pedidos[id_dic][2]
+
+        if peso_actual <= capcidad:
+            beneficio_con, lista_con = seleccion_pedidos_dinamico(id_dic + 1, capcidad-peso_actual)
+            beneficio_con += beneficio_actual
+            # Comparamos cuál de las dos opciones es mejor
+            if beneficio_con > beneficio_sin:
+                res = (beneficio_con, [pedidos[id_dic]] + lista_con)
+            else:
+                res = (beneficio_sin, lista_sin)
+        else:
+            res = (beneficio_sin, lista_sin)
+
+        # 5. Guardar en cache la solución y retornar
+        cache[existe] = res
+        return res
+
+    total_beneficio, seleccionados = seleccion_pedidos_dinamico(0, capacidad)
+    return seleccionados
+
+
+
+
+
+
+#prueba
+#Pedido = (id, peso, beneficio, destino)
+pedido1 = [0, 50, 250, "A"]
+pedido2 = [1, 25, 20, "A"]
+pedido3 = [2, 45, 21, "A"]
+pedido4 = [3, 1, 100, "A"]
+pedido5 = [4, 1, 100, "A"]
+pedido6 = [5, 1, 100, "A"]
+pedido7 = [6, 1, 100, "A"]
+pedido8 = [7, 1, 100, "A"]
+pedido9 = [8, 20, 20, "A"]
+
+pedidos = [pedido1, pedido2, pedido3, pedido4, pedido5, pedido6,pedido7, pedido8, pedido9]
+
+print(seleccion_pedidos(pedidos,100))
--- src/main.py
+++ src/main.py
@@ -2,24 +2,21 @@
 Este es el modulo principal de la aplicacion, este contiene:
     La funcion main encargada de llamar a las demas funciones y ejecutar el programa.
     ...
-    
-"""
 
-"""
 Estructuras de datos y algoritmos implementados:
 
     Estructuras de datos:
         - Matriz de pedidos (Nx3 pedidos) :
             + Fila: Pedido 
-            + Columnas: id_pedido, peso, beneficio
+            + Columnas: id_pedido, peso, beneficio, destino
         
-        - Capacidad de carga: C (entero) representando la capacidad máxima de carga de cada vehículo.
+        - Capacidad de carga: C capacidad máxima de carga de vehículo.
 
-        - Diccionario de camiones y pedidos asignados: ???
-            + Clave: id_camion 
-            + Valor: Tupla "Tripelata -> + punto de entrega"
-                * Beneficio total
-                * Lista de pedidos asignados a ese camión.
+        -Lista de pedidos asignados:
+            + Valor: Tupla 3 valors
+                * Id_Pedido
+                * Destino
+                * Beneficio
         
         - Matriz de distancias entre puntos de entrega (grafo ponderado) {k*k nodos}:
             + Fila: Punto o nodo de entrega
